d87d98f6982ce9fa0bf3e9af6cda190a6c5b121d
tungliang <tungliang.hannus@gmail.com>
2025-04-19 12:01:41 +0800

    chore: modified config

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
     server: {
-        preset: "deno_deploy",
         rollupConfig: {
           external: ["node:async_hooks"],
         },
