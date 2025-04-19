fcac1bd7fe740f8d9b1f26b1ab15086455aa30ee
tungliang <tungliang.hannus@gmail.com>
2025-04-19 16:29:49 +0800

    fix: moved the @deno/kv to dev mode. in the prod mode, use Deno.openKv()

--- src/api/database/action.ts
+++ src/api/database/action.ts
@@ -1,6 +1,7 @@
-import { openKv } from "@deno/kv";
+// import { openKv } from "@deno/kv";
 import type { KvUser } from "../../lib/types";
 import { action } from "@solidjs/router";
+import { openKvDev } from "../../lib/kv";
 
 /**
  * 向 KV 数据库添加/更新用户信息（email 和 password）
@@ -12,12 +13,7 @@ export const setKvUser = action(async function (
 ): Promise<boolean> {
   "use server";
   try {
-    // prod mode, do not use access token
-    const kv = await openKv();
-    // dev mode, use access token
-    // const kv = await openKv("https://api.deno.com/databases/7f9aac9f-cce2-4148-9cd5-1d30bcd0a1d2/connect");
-
-    console.log(kv);
+    const kv = await openKvDev();
     await kv.set(["email"], formData.get("email"));
     await kv.set(["password"], formData.get("password"));
     return true;
--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -1,12 +1,10 @@
 // 示例：Deno KV 读取用户信息 API
 // 参考 https://docs.deno.com/deploy/kv/manual/
 
-import { openKv } from "@deno/kv";
-
+// import { openKv } from "@deno/kv";
 import { KvUser } from "../../lib/types";
+import { openKvDev } from "../../lib/kv";
 
-// TODO: 替换为你的 Deno KV 数据库 uuid
-const KV_NAMESPACE = "<YOUR_KV_UUID>";
 
 /**
  * 读取 KV 数据库中的用户信息（email 和 password）
@@ -16,7 +14,7 @@ export async function getKvUser(): Promise<KvUser | null> {
   "use server";
   // 打开 KV 数据库
   // const kv = await openKv("https://api.deno.com/databases/7f9aac9f-cce2-4148-9cd5-1d30bcd0a1d2/connect");
-  const kv = await openKv();
+  const kv = await openKvDev();
   // 读取 email 和 password
   const [emailRes, passwordRes] = await Promise.all([
     kv.get(["email"]),
